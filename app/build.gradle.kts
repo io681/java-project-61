35142d2046d1da69be124c9f8067e512da8cfaf8
io681 <io-68@yandex.ru>
2023-09-18 15:25:18 +0300

    add application and options.release in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    application
 }
 
 group = "hexlet.code"
@@ -17,3 +18,13 @@ dependencies {
 tasks.test {
     useJUnitPlatform()
 }
+
+
+application {
+    mainClass.set("hexlet.code.App")
+}
+
+tasks.compileJava {
+    options.release = 20
+}
+
