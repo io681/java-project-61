911efcbc262a27b91da6a440cd13c1fe6e565713
io681 <io-68@yandex.ru>
2023-10-02 11:36:09 +0300

    change generate Progressive Array

--- app/src/main/java/hexlet/code/games/GameProgression.java
+++ app/src/main/java/hexlet/code/games/GameProgression.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 
 public class GameProgression extends Engine {
 
-    private String valueHiddenNumber;
+    private int valueHiddenNumber;
 
     public static final int RANGE_START_LENGTH_PROGRESSION = 5;
     public static final int RANGE_END_LENGTH_PROGRESSION = 10;
@@ -21,41 +21,57 @@ public class GameProgression extends Engine {
     }
 
     public  final void createQuestion() {
-        System.out.println("Question: " + createProgressionRandom());
+        String resultProgressionForQuestion = String
+                .join(" ",makeProgressionWithHiddenElementForQuestion(generateProgressionRandom()));
+        System.out.println("Question: " + resultProgressionForQuestion);
     }
 
     public  final boolean checkAnswer() {
-        return getAnswer().equals(getValueHiddenNumber());
+        return Integer.parseInt(getAnswer()) == getValueHiddenNumber();
     }
 
-    public final String createProgressionRandom() {
+    public final int[] generateProgressionRandom() {
         Utils utils = new Utils();
 
         int startNumberProgression = getGeneratedNumber(0);
         int stepProgression = getGeneratedNumber(1);
         int lengthProgression = utils.createNumberInRange(RANGE_START_LENGTH_PROGRESSION,
                 RANGE_END_LENGTH_PROGRESSION);
+        int[] progressionNumbers = new int[lengthProgression];
 
-        String[] progressionNumbers = new String[lengthProgression];
-        int positionHidden = utils.createNumberInRange(0, lengthProgression - 1);
-        int nextNumber = 0;
+        int nextNumberProgression = startNumberProgression;
 
         for (var i = 0; i < lengthProgression; i++) {
-            progressionNumbers[i] = Integer.toString(startNumberProgression + nextNumber);
-            nextNumber += stepProgression;
+            progressionNumbers[i] = nextNumberProgression;
+            nextNumberProgression += stepProgression;
         }
 
-        setValueHiddenNumber(progressionNumbers[positionHidden]);
-        progressionNumbers[positionHidden] = "..";
-
-        return String.join(" ", progressionNumbers);
+        return progressionNumbers;
     }
 
-    public final String getValueHiddenNumber() {
+    public final String[] makeProgressionWithHiddenElementForQuestion(int[] progressionArray) {
+        Utils utils = new Utils();
+        int positionHidden = utils.createNumberInRange(0, progressionArray.length - 1);
+        setValueHiddenNumber(progressionArray[positionHidden]);
+
+        String[] progressionArrayStringWithHiddenElement = new String[progressionArray.length];
+
+        for (var i = 0; i < progressionArray.length; i++) {
+            if (i == positionHidden) {
+                progressionArrayStringWithHiddenElement[i] = "..";
+                continue;
+            }
+            progressionArrayStringWithHiddenElement[i] = Integer.toString(progressionArray[i]);
+        }
+
+        return progressionArrayStringWithHiddenElement;
+    }
+
+    public final int getValueHiddenNumber() {
         return valueHiddenNumber;
     }
 
-    public final void setValueHiddenNumber(String valueHiddenNumberForSet) {
+    public final void setValueHiddenNumber(int valueHiddenNumberForSet) {
         this.valueHiddenNumber = valueHiddenNumberForSet;
     }
 
